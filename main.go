7f19286563eb83e50fbe99f57a27d4c337c1a463
NghiaLQ <nghialq@vng.com.vn>
2019-10-20 17:23:51 +0700

    Add upload image endpoint

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"log"
 	"net/http"
@@ -12,7 +13,14 @@ import (
 	_ "github.com/jinzhu/gorm"
 
 	"encoding/json"
+
+	"io/ioutil"
+	"mime/multipart"
 )
+
+// todo get this from config file
+const ClientID = "5958ddd634f2aea"
+const AccessToken = "567d191b832101282951460d490181a4ca8eb3e9"
 
 //shit
 type ProjectPost struct {
@@ -45,6 +53,73 @@ func testPostHandler(w http.ResponseWriter, r *http.Request) {
 
 }
 
+func uploadImageHandler(w http.ResponseWriter, r *http.Request) {
+
+	fmt.Println("uploadImageHandler hit")
+	r.ParseMultipartForm(10 << 20)
+
+	file, fileHeader, err := r.FormFile("image")
+	if err != nil {
+		fmt.Println("Error when upload file")
+		fmt.Println(err)
+		return
+	}
+	defer file.Close()
+
+	fmt.Printf("Uploaded file %s\n", fileHeader.Filename)
+	fmt.Printf("File size=%v", fileHeader.Size)
+
+	//todo upload to imgur
+}
+
+func testUploadImgur(w http.ResponseWriter, r *http.Request) {
+	file, err := ioutil.ReadFile("assets/57232103_572879629870418_546118032422862848_n.jpg")
+	check(err)
+	w.Header().Set("Content-Type", "application/json")
+	fmt.Println(w.Header())
+	res := uploadImageToImgur(file)
+	enc := json.NewEncoder(w)
+	enc.Encode(&res)
+
+}
+func uploadImageToImgur(image []byte) map[string]interface{} {
+	var requestBody bytes.Buffer
+
+	multipartWriter := multipart.NewWriter(&requestBody)
+
+	imageFieldWriter, err := multipartWriter.CreateFormField("image")
+
+	check(err)
+	_, err = imageFieldWriter.Write(image)
+	check(err)
+
+	multipartWriter.Close()
+
+	req, err := http.NewRequest("POST", "https://api.imgur.com/3/upload", &requestBody)
+	check(err)
+
+	req.Header.Set("Authorization", fmt.Sprint("Client-ID ", ClientID))
+	req.Header.Set("Content-Type", multipartWriter.FormDataContentType())
+	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", AccessToken))
+
+	response, err := http.DefaultClient.Do(req)
+	check(err)
+
+	var result map[string]interface{}
+	json.NewDecoder(response.Body).Decode(&result)
+
+	fmt.Println(result)
+
+	return result
+
+}
+
+func check(err error) {
+	if err != nil {
+		log.Fatal(err)
+		panic(err)
+	}
+}
 func main() {
 	db := setupDB()
 	//p := ProjectPost{Title: "công trình abc", Body: "adsfas fsa fas dfas df as"}
@@ -54,9 +129,10 @@ func main() {
 	fmt.Println(pselect)
 	router := mux.NewRouter().StrictSlash(true)
 	router.HandleFunc("/ping", pingHandler)
-	router.HandleFunc("/testPost", testPostHandler).Methods("POST")
+	router.HandleFunc("/test/post", testPostHandler).Methods("POST")
+	router.HandleFunc("/test/upload", testUploadImgur).Methods("GET")
+	router.HandleFunc("/images/upload", uploadImageHandler).Methods("POST")
 
 	fmt.Println("Server running at :8000")
 	log.Fatal(http.ListenAndServe(":8000", router))
-
 }
