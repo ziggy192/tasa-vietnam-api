332e56c32e055a3851f83c63c3e90c85d6c134b8
NghiaLQ <nghialq@vng.com.vn>
2019-10-20 14:51:59 +0700

    json package

--- main.go
+++ main.go
@@ -4,18 +4,24 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"time"
 
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm"
+
+	"encoding/json"
 )
 
 //shit
 type ProjectPost struct {
-	gorm.Model
-	Title string
-	Body  string
+	ID        uint `gorm:"primary_key"`
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	Title     string
+	Body      string
+	Tags      string
 }
 
 func pingHandler(w http.ResponseWriter, r *http.Request) {
@@ -26,8 +32,19 @@ func setupDB() *gorm.DB {
 	db, _ := gorm.Open("mysql", "root:12345678@/tasa?charset=utf8&parseTime=True&loc=Local")
 
 	return db
+}
+
+func testPostHandler(w http.ResponseWriter, r *http.Request) {
+
+	dec := json.NewDecoder(r.Body)
+	enc := json.NewEncoder(w)
+
+	var v map[string]interface{}
+	dec.Decode(&v)
+	enc.Encode(&v)
 
 }
+
 func main() {
 	db := setupDB()
 	//p := ProjectPost{Title: "công trình abc", Body: "adsfas fsa fas dfas df as"}
@@ -37,6 +54,9 @@ func main() {
 	fmt.Println(pselect)
 	router := mux.NewRouter().StrictSlash(true)
 	router.HandleFunc("/ping", pingHandler)
+	router.HandleFunc("/testPost", testPostHandler).Methods("POST")
+
+	fmt.Println("Server running at :8000")
 	log.Fatal(http.ListenAndServe(":8000", router))
 
 }
