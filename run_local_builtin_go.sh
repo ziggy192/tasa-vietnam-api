3db9d654dadfd3357b1e82a17bff88f64e252765
NghiaLQ <nghialq@vng.com.vn>
2019-11-17 19:38:39 +0700

    add script for running local without docker

---
+++ run_local_builtin_go.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+DB_ADDRESS=localhost:3306
+DB_USERNAME=root
+DB_PASSWORD=12345678
+
+go build 
+./tasa-vietnam-api -dbAddress=$DB_ADDRESS -username=$DB_USERNAME -password=$DB_PASSWORD
