f8ca2bf6e932dae6f2b7aca55c31d04876dc1f62
NghiaLQ <nghialq@vng.com.vn>
2019-12-07 13:14:11 +0700

    add setup_mysql script

--- setup_mysql_database.sh
+++ setup_mysql_database.sh
@@ -1,1 +1,3 @@
-docker run --name=mysql-tasa -d  -e MYSQL_USER=remote -e MYSQL_PASSWORD=12345678 -e MYSQL_DATABASE=tasa -e MYSQL_ROOT_PASSWORD=12345678 mysql/mysql-server:8.0 
+#!/bin/bash
+
+docker run --name=mysql-tasa -d -p 3306:3306 -e MYSQL_USER=remote -e MYSQL_PASSWORD=12345678 -e MYSQL_DATABASE=tasa -e MYSQL_ROOT_PASSWORD=12345678 mysql/mysql-server:8.0 
